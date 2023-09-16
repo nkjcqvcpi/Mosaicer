883aa8ad63c21a1434a16b4b5aae89615bc0645f
nkjcqvcpi <lhtt.100@163.com>
2023-09-16 16:25:30 +0800

    0.1.0

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
-# Mosaicer
+# A bunch of toolkit for illustration 
 
-A tiny Python tool to add the black occlusion required by pixiv to the work manually.
+A bunch of tiny Python tools to edit the illustration work.
 
+### Mosaicer:
+To add the black occlusion required by pixiv to the work manually.
 - Auto calculate the width of the rectangular mosaic.
 - Drag mouse to draw.
+
+~~### Resizer:
+To resize the image by drag and drop.~~
+
+### Watermarker:
+To add the watermarks to the work manually.
+- Load the watermark by add the path into config.toml
+
+### Thumbnailer:
+Based on previous Resizer tool, but with a 720px long side.
 
 ## Release Note:
 
 #### Ver. 0.0.1 (2023-09-02)
 
-Initialization
+Initialization.
 
+#### Ver. 0.0.2 (2023-09-08)
+
+Add Resizer.
+
+#### Ver. 0.1.0 (2023-09-16)
+
+Change Resizer to Thumbnailer.
+Combine all tools into one python file.
--- main.py
+++ main.py
@@ -1,102 +1,257 @@
+import os
+from collections import OrderedDict
+import logging
+from math import ceil
+import tomllib
 from tkinter import *
 from tkinter import messagebox, filedialog
 from tkinter.ttk import *
-from collections import OrderedDict
 
-from PIL import ImageTk, ImageDraw, Image
-
-from math import ceil
+from PIL import ImageTk, ImageDraw, Image, UnidentifiedImageError
 
 
-VERSION = '0.0.1'
-GR = 0.618
+class IlluToolbox(Tk):
+    with open("config.toml", "rb") as f:
+        cf = tomllib.load(f)
 
-
-class Mosaicer(Tk):
     def __init__(self):
         super().__init__()
         self.res = (self.winfo_screenwidth(), self.winfo_screenheight())
-        self.o = self.res
-        self.scale = 1
-        self.linewidth = int(max(self.res) * 0.01)
-        self.s_size = (int(self.winfo_screenwidth() * 0.8), int(self.winfo_screenheight() * 0.8))
-        self.rt = []
-        self.draft = []
-        self.mosaics = OrderedDict()
+
+        self.str_mode = StringVar()
+        self.str_mouse = StringVar()
+        self.str_list = StringVar()
+        self.str_status = StringVar()
+
+        self.od_mosaics = OrderedDict()
+        self.od_files = OrderedDict()
+        self.od_watermarks = OrderedDict()
+
+        self.wm = Image.open(self.cf['WM'])
+
+        self.button_open = Button(self, text='Open', command=self.open_dir, width=16)
+        self.button_save = Button(self, text='Save & Close', command=self.save_file, width=16)
+        self.button_thumb = Button(self, text='Thumbnail', command=self.save_thumbnail, width=16)
+        self.canvas_editor = Canvas(self)
+        self.mode_mosaicer = Radiobutton(self, text='Mosaicer', variable=self.str_mode, value='mosaicer')
+        self.mode_watermarker = Radiobutton(self, text='Watermarker', variable=self.str_mode, value='watermarker')
+        self.label_mouse = Label(self, textvariable=self.str_mouse)
+        self.label_status = Label(self, textvariable=self.str_status, borderwidth=1, relief=SUNKEN, anchor=W)
+        self.listbox_files = Listbox(self, listvariable=self.str_list)
+        # self.combobox_res = Combobox(self)
+
         self.menu()
+
+        self.scale_img = 1
+        self.scale_wm = 1
+        self.width_line = 0
+        self.start_pt = (0, 0)
+        self.draft = []
+
+        # self.combobox_res['value'] = ('Original', '720P', '1080P', '4K', '8K')
+        self.res_list = {'720P': 720, '1080P': 1080, '4K': 2160, '8K': 4320}
+
+        self.win_size = (self.winfo_width(), self.winfo_height())
+
+    def __call__(self, *args, **kwargs):
+        self.str_mode.set('mosaicer')
+        # self.combobox_res.current(0)
+        self.str_status.set('Done')
+        self.widget_place()
+        self.widget_bind()
         self.set_win()
         self.mainloop()
 
-    def open_file(self):
-        self.filename = filedialog.askopenfilename()
-        self.img = Image.open(self.filename)
-        self.o = self.img.size
-        if self.o[0] >= self.o[1]:
-            s_width = min(self.s_size[0], self.o[0])
-            self.scale = s_width / self.o[0]
-            display_size = (s_width, int(self.scale * self.o[1]))
+    def open_dir(self):
+        if dirs := filedialog.askdirectory():
+            for root, dirs, files in os.walk(dirs):
+                for fn in files:
+                    fp = os.path.join(root, fn)
+                    try:
+                        img = Image.open(fp)
+                    except UnidentifiedImageError:
+                        continue
+                    self.od_files[fn] = img
+            self.str_list.set(' '.join(self.od_files.keys()))
+            self.str_status.set(f'Open Dir: Open {len(self.od_files)} images success!')
+            self.listbox_files.update()
+            self.listbox_files.focus_set()
+            self.listbox_files.select_set(0)
+            self.listbox_files.event_generate('<<ListboxSelect>>')
+
+    def open_files(self):
+        if files := filedialog.askopenfilenames():
+            for fp in files:
+                try:
+                    img = Image.open(fp)
+                except UnidentifiedImageError:
+                    raise Warning(f'UnidentifiedImageError: {fp}')
+                _, fn = os.path.split(fp)
+                self.od_files[fn] = img
+            self.str_list.set(' '.join(self.od_files.keys()))
+            self.str_status.set(f'Open Files: {len(self.od_files)} images success!')
+            self.listbox_files.update()
+            self.listbox_files.focus_set()
+            self.listbox_files.select_set(0)
+            self.listbox_files.event_generate('<<ListboxSelect>>')
+
+    def canvas_show(self, event):
+        if len(self.od_files) == 0:
+            return
+
+        self.od_mosaics, self.od_watermarks = OrderedDict(), OrderedDict()
+        img = self.od_files[self.listbox_files.get(self.listbox_files.curselection())]
+        canvas_width, canvas_height = int(self.winfo_width() * 0.9), int(self.winfo_height() * 0.9)
+
+        ratio_aspect_ori = img.width / img.height
+        ratio_aspect_can = canvas_width / canvas_height
+
+        scale_width = canvas_width / img.width
+        scale_height = canvas_height / img.height
+
+        if ratio_aspect_ori > ratio_aspect_can:
+            display_size = (canvas_width, int(img.height * scale_width))
+            self.scale_wm = int(img.height * scale_width) / 720
+            self.scale_img = scale_width
+        elif ratio_aspect_ori <= ratio_aspect_can:
+            display_size = (int(img.width * scale_height), canvas_height)
+            self.scale_wm = int(img.width * scale_height) / 720
+            self.scale_img = scale_height
         else:
-            s_height = min(self.s_size[1], self.o[1])
-            self.scale = s_height / self.o[1]
-            display_size = (int(self.scale * self.o[0]), s_height)
+            display_size = (canvas_width, canvas_height)
+            self.scale_wm = canvas_width / 1280
+            self.scale_img = scale_width
 
-        self.linewidth = ceil(0.01 * max(display_size))
+        self.width_line = ceil(0.01 * max(display_size))
 
-        img = self.img.resize(display_size)
+        wm = self.wm.resize((int(self.wm.width * self.scale_wm), int(self.wm.height * self.scale_wm)))
+        global wm_dis, img_dis
+        wm_dis = ImageTk.PhotoImage(wm)
+        img_dis = ImageTk.PhotoImage(img.resize(display_size))
 
-        global photo
-        photo = ImageTk.PhotoImage(img)
-        self.button.pack_forget()
-        self.save_button.pack(anchor='center')
-        self.geometry(f'{display_size[0]}x{display_size[1] + 10}+{int((self.res[0] - display_size[0]) / 2)}+{int((self.res[1] - display_size[1]) / 2)}')
-        self.c_picture.config(height=display_size[1], width=display_size[0])
-        self.c_picture.pack(anchor='center')
-        self.c_picture.delete('all')
-        self.c_picture.create_image(0, 0, image=photo, anchor=NW)
+        self.canvas_editor.delete('all')
+        self.canvas_editor.config(height=display_size[1], width=display_size[0])
+        self.canvas_editor.create_image(0, 0, image=img_dis, anchor=NW)
 
-    def render(self):
-        draw = ImageDraw.Draw(self.img)
-        for _, line in self.mosaics.items():
-            line = [i / self.scale for i in line]
-            draw.line(line, width=ceil(self.linewidth / self.scale), fill=0)
+    def draw_mosaic(self, img):
+        draw = ImageDraw.Draw(img)
+        for _, line in self.od_mosaics.items():
+            line = [i / self.scale_img for i in line]
+            draw.line(line, width=ceil(self.width_line / self.scale_img), fill=0)
+        return img
 
-    def save_file(self):
-        fn, pf = self.filename.split('.')
-        self.render()
-        self.img.save(fn + '_mosaic.' + pf)
-        self.reset()
+    def mosaic_confirm(self, event):
+        self.canvas_editor.delete(*self.draft)
+        lid = self.canvas_editor.create_line(*self.start_pt, event.x, event.y, width=self.width_line)
+        self.od_mosaics[lid] = (self.start_pt[0], self.start_pt[1], event.x, event.y)
+        self.canvas_editor.update()
+
+    def mosaic_preview(self, event):
+        self.canvas_editor.delete(*self.draft)
+        lid = self.canvas_editor.create_line(*self.start_pt, event.x, event.y,
+                                             width=self.width_line, dash=[self.width_line])
+        self.draft.append(lid)
+        self.canvas_editor.update()
+
+    # def load_watermark(self):
+
+    def watermark_preview(self, event):
+        self.canvas_editor.delete(*self.draft)
+        wmp = self.canvas_editor.create_image(event.x, event.y, image=wm_dis, anchor=CENTER)
+        self.draft.append(wmp)
+        self.canvas_editor.update()
+
+    def watermark_confirm(self, event):
+        self.canvas_editor.delete(*self.draft)
+        wmp = self.canvas_editor.create_image(event.x, event.y, image=wm_dis, anchor=CENTER)
+        self.od_watermarks[wmp] = (int(event.x - wm_dis.width() / 2), int(event.y - wm_dis.height() / 2))
+        self.canvas_editor.update()
+
+    def draw_watermark(self, img):
+        for pos in self.od_watermarks.values():
+            wm = self.wm.resize((int(self.wm.width * self.scale_wm / self.scale_img),
+                                 int(self.wm.height * self.scale_wm / self.scale_img)))
+            img.paste(wm, (int(pos[0] / self.scale_img), int(pos[1] / self.scale_img)), wm)
+        return img
+
+    # def resize(self):
+    #     if self.img.width >= self.img.height:  # horizontal
+    #         scale = self.res_list[self.combobox_res.get()] / self.img.height
+    #         new_height = self.res_list[self.combobox_res.get()]
+    #         new_width = int(scale * self.img.width)
+    #     else:  # vertical
+    #         scale = self.res_list[self.combobox_res.get()] / self.img.width
+    #         new_height = int(scale * self.img.height)
+    #         new_width = self.res_list[self.combobox_res.get()]
+    #     self.img = self.img.resize((new_width, new_height))
+
+    def save_file(self, filename=None):
+        curr_sel = self.listbox_files.curselection()
+        img = self.od_files.pop(self.listbox_files.get(curr_sel))
+        fp, fn = os.path.split(img.filename)
+        fp = fp if filename is None else filename
+        fn, ext = os.path.splitext(fn)
+
+        if len(self.od_mosaics) != 0:
+            img = self.draw_mosaic(img)
+            fn += '_m'
+
+        if len(self.od_watermarks) != 0:
+            img = self.draw_watermark(img)
+            fn += '_w'
+
+        # if self.combobox_res.get() != 'Original':
+        #     self.resize()
+        #     fn += '_r'
+
+        img.save(os.path.join(fp, fn + ext), quality='keep', optimize=True, progressive=True)
+
+        self.canvas_editor.delete('all')
+        self.str_list.set(' '.join(self.od_files.keys()))
+        self.listbox_files.update()
+        self.listbox_files.select_set(curr_sel)
+        self.listbox_files.event_generate('<<ListboxSelect>>')
+
+    def save_thumbnail(self):
+        img = self.od_files[self.listbox_files.get(self.listbox_files.curselection())]
+        thumb = img.copy()
+        fn, ext = os.path.splitext(img.filename)
+
+        if len(self.od_mosaics) != 0:
+            thumb = self.draw_mosaic(thumb)
+            fn += '_m'
+
+        if len(self.od_watermarks) != 0:
+            thumb = self.draw_watermark(thumb)
+            fn += '_w'
+
+        fn += '.thumbnail'
+        thumb.thumbnail((720, 720))
+        thumb.save(fn + ext, optimize=True, progressive=True)
 
     def save_as_file(self):
-        f = filedialog.asksaveasfilename()
-        # TODO
+        fn = filedialog.asksaveasfilename()
+        self.save_file(fn)
 
-    def undo():
+    def undo(self):
         pass
         # TODO
 
-    def reset(self):
-        w = int(self.res[0] * GR)
-        h = int(self.res[1] * GR)
-        self.geometry(f'{w}x{h}+{int((self.res[0] - w) / 2)}+{int((self.res[1] - h) / 2)}')
-        self.button.pack(anchor='center')
-        self.save_button.pack_forget()
-        self.c_picture.pack_forget()
-
     def menu(self):
         def about():
-            messagebox.showinfo("About", "Version：" + VERSION)
+            messagebox.showinfo("About", "Version：" + self.cf['VERSION'])
 
         m_main = Menu(self)
 
         app_menu = Menu(m_main, name='apple')
         app_menu.add_command(label='About', command=about)
         m_main.add_cascade(menu=app_menu)
-    
-        filemenu = Menu(m_main, tearoff=0)
-        filemenu.add_command(label="Open", command=self.open_file)
-        filemenu.add_command(label="Save", command=self.save_file)
-        filemenu.add_command(label="Save as...", command=self.save_as_file)
-        m_main.add_cascade(label="File", menu=filemenu)
+
+        file_menu = Menu(m_main, tearoff=0)
+        file_menu.add_command(label="Open", command=self.open_files)
+        file_menu.add_command(label="Save", command=self.save_file)
+        file_menu.add_command(label="Save as...", command=self.save_as_file)
+        m_main.add_cascade(label="File", menu=file_menu)
 
         editmenu = Menu(m_main, tearoff=0)
         editmenu.add_command(label="Undo", command=self.undo)
@@ -104,43 +259,48 @@ class Mosaicer(Tk):
 
         self.config(menu=m_main)
 
-    def MousePress(self, event):
+    def mouse_press(self, event):
         self.start_pt = (event.x, event.y)
+        if self.str_mode.get() == 'watermarker':
+            self.watermark_confirm(event)
 
-    def MouseRelease(self, event):
-        self.c_picture.delete(*self.draft)
-        lid = self.c_picture.create_line(*self.start_pt, event.x, event.y, width=self.linewidth)
-        self.mosaics[lid] = (self.start_pt[0], self.start_pt[1], event.x, event.y)
-        self.c_picture.update()
-
-    def MouseDrag(self, event):
-        self.c_picture.delete(*self.draft)
-        lid = self.c_picture.create_line(*self.start_pt, event.x, event.y, width=self.linewidth, dash=self.linewidth)
-        self.draft.append(lid)
-        self.c_picture.update()
-
-    # def MouseClick(self, event):
-    #     self.rt.append((event.x, event.y))
-    #     self.c_picture.create_oval(event.x, event.y, width=self.w)
-    #     if len(self.rt) == 4:
-    #         self.c_picture.create_polygon(self.rt, fill='pink')
-    #         self.rt = []
-    #     self.c_picture.update()
+    def mouse_position(self, event):
+        self.str_mouse.set(f'x: {event.x} y: {event.y}')
+        if self.str_mode.get() == 'watermarker':
+            self.watermark_preview(event)
 
     def set_win(self):
         self.lift()
         self.attributes('-topmost', True)
         self.after_idle(self.attributes, '-topmost', False)
-        self.title("Mosaicer")
+        self.geometry(f'{self.res[0]}x{self.res[1]}+0+0')
+        self.title("illuToolbox")
         self.resizable(True, True)
-        self.button = Button(self, text='Open', command=self.open_file)
-        self.save_button = Button(self, text='Save', command=self.save_file)
-        self.c_picture = Canvas(self, borderwidth=0, relief="solid")
-        # self.c_picture.bind('<Button-1>', self.MouseClick)
-        self.c_picture.bind('<ButtonPress-1>', self.MousePress)
-        self.c_picture.bind('<ButtonRelease-1>', self.MouseRelease)
-        self.c_picture.bind('<B1-Motion>', self.MouseDrag)
-        self.reset()
+
+    def widget_place(self):
+        self.button_open.place(relx=.15, rely=0.01, anchor=N)
+        self.button_save.place(relx=.3, rely=0.01, anchor=N)
+        self.button_thumb.place(relx=.45, rely=0.01, anchor=N)
+        # self.combobox_res.place(relwidth=.1, relheight=.05, relx=.45, rely=0, anchor=N)
+        self.mode_mosaicer.place(relwidth=.1, relheight=.05, relx=.6, rely=0, anchor=N)
+        self.mode_watermarker.place(relwidth=.1, relheight=.05, relx=.75, rely=0, anchor=N)
+        self.label_mouse.place(relwidth=.1, relheight=.05, relx=.9, rely=0, anchor=N)
+        self.label_status.place(relwidth=1, relheight=.05, relx=0, rely=.95, anchor=NW)
+        self.listbox_files.place(relwidth=.1, relheight=.9, relx=.9, rely=.05, anchor=NW)
+        self.canvas_editor.place(relx=.45, rely=.5, anchor=CENTER)
+
+
+    def widget_bind(self):
+        self.canvas_editor.bind('<ButtonPress-1>', self.mouse_press)
+        self.canvas_editor.bind('<ButtonRelease-1>',
+                                lambda e: self.mosaic_confirm(e) if self.str_mode.get() == 'mosaicer' else None)
+        self.canvas_editor.bind('<B1-Motion>',
+                                lambda e: self.mosaic_preview(e) if self.str_mode.get() == 'mosaicer' else None)
+        self.canvas_editor.bind('<Motion>', self.mouse_position)
+        self.listbox_files.bind("<<ListboxSelect>>", self.canvas_show)
+        self.canvas_editor.bind('<Configure>', self.canvas_show)
+
 
 if __name__ == '__main__':
-    Mosaicer()
+    window = IlluToolbox()
+    window()
